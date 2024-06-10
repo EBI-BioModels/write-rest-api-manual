4cb70c499fe37b630cbbf623841199deb81257fb
Tung Nguyen <nvntung@gmail.com>
2024-06-10 16:19:14 +0100

    Parameterised using variables

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,18 @@
 # local tokens
 ACCESS_TOKEN=$ACCESS_TOKEN_LOCAL_ADMIN
 
+PREFIX_DOWNUP_SVR="http://localhost:7000" # local instance
+# PREFIX_DOWNUP_SVR="https://wwwdev.ebi.ac.uk" # dev instance
+# PREFIX_DOWNUP_SVR="https://www.ebi.ac.uk" # prod instance
+
+UPLOAD_SERVER_URL="$PREFIX_DOWNUP_SVR/biomodels/services/download/upload"
+
+BM_SVR="http://localhost:8080/biomodels" # local instance
+# BM_SVR="https://wwwdev.ebi.ac.uk/biomodels" # dev instance
+# BM_SVR="https://www.ebi.ac.uk/biomodels" # prod instance
+BM_SERVER_CREATE_SUBMISSION_URL="$BM_SVR/submission/create"
+BM_SERVER_UPDATE_SUBMISSION_URL="$BM_SVR/submission/update"
+
 echo "SUBMITTING A MODEL"
 SUBMISSION_FOLDER=$(python3 -c "import uuid; print(str(uuid.uuid4()))")
 echo "$SUBMISSION_FOLDER" 
@@ -13,7 +25,7 @@ curl -s -X POST \
   -H "SubmissionFolder: $SUBMISSION_FOLDER" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   -F file=@"$file" \
-  http://localhost:7000/biomodels/services/download/upload | jq
+  $UPLOAD_SERVER_URL | jq
 done
 
 echo "CREATING A NEW MODEL OR UPDATING AN EXISTING MODEL"
@@ -22,5 +34,5 @@ curl -s -X POST \
   -H "SubmissionFolder: $SUBMISSION_FOLDER" \
   -H "Content-Type: application/json" \
   --data-binary @submission-metadata.json \
-  http://localhost:8080/biomodels/api/submission/create\?format\=json | jq
+  $BM_SERVER_UPDATE_SUBMISSION_URL\?format\=json | jq
 
