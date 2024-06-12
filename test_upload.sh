f7e1f86b06d68210bffe8ac3917eafa16e2f2eee
Tung Nguyen <nvntung@gmail.com>
2024-06-12 16:34:18 +0100

    Test uploading service

---
+++ test_upload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+ACCESS_TOKEN=$(echo $ACCESS_TOKEN_DEV_TUNG)
+WWW=wwwdev
+#WWW=www
+
+# Upload the model files
+SUBMISSION_FOLDER=$(python3 -c "import uuid; print(str(uuid.uuid4()))")
+echo $SUBMISSION_FOLDER
+
+curl -X POST \
+  -H "SubmissionFolder: $SUBMISSION_FOLDER" \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -F file=@/Users/tnguyen/Downloads/Stucki2005.xml \
+  https://$WWW.ebi.ac.uk/biomodels/services/upload
