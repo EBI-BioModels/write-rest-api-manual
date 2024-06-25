268b1c2406f11ef4f214e76e0454cf5d6304ba51
Tung Nguyen <nvntung@gmail.com>
2024-06-25 16:03:21 +0100

    Format and refactor the entire file

--- pyguide/run_update.py
+++ pyguide/run_update.py
@@ -18,32 +18,33 @@ prod_biomodels = "https://wwwdev.ebi.ac.uk/biomodels/"
 root_biomodels = "http://localhost:8080/biomodels/"
 
 
-def getNewMetadata(biomd, oldmetadata, master):
+def get_new_metadata(biomd_id, old_metadata, master):
     metadata = {}
     for key in ["name", "description", "publication"]:
-        metadata[key] = oldmetadata[key]
-    metadata["submissionId"] = oldmetadata["submissionId"]
-    metadata["publicationId"] = oldmetadata["publicationId"]
-    metadata["readme_submission"] = "Curation updates for SED-ML and validity, from the Center for Reproducible Biomedical Modeling, via Lucian Smith."
+        metadata[key] = old_metadata[key]
+    metadata["submissionId"] = old_metadata["submissionId"]
+    metadata["publicationId"] = old_metadata["publicationId"]
+    metadata[
+        "readme_submission"] = "Curation updates for SED-ML and validity, from the Center for Reproducible Biomedical Modeling, via Lucian Smith."
     metadata["other_info"] = "SBML Model Format"
     metadata["isMetadataSubmission"] = False
     metadata["isAmend"] = True
     metadata["comment"] = "A variety of manual and automated updates for all extant curated biomodels, 2024."
-    metadata["format"] = oldmetadata["format"]
+    metadata["format"] = old_metadata["format"]
     metadata["files"] = {"main": [], "additional": []}
-    if "modellingApproach" in oldmetadata and oldmetadata["modellingApproach"] is not None:
-        metadata["modelling_approach"] = oldmetadata["modellingApproach"]["name"]
+    if "modellingApproach" in old_metadata and old_metadata["modellingApproach"] is not None:
+        metadata["modelling_approach"] = old_metadata["modellingApproach"]["name"]
 
     # Now update all the files:
     oldfiles = {}
     for key in ["main", "additional"]:
-        for entry in oldmetadata["files"][key]:
+        for entry in old_metadata["files"][key]:
             oldfiles[entry["name"]] = entry["description"]
-    
+
     newfiles = []
-    for root, dirs, files in os.walk(final_dir + biomd):
+    for root, dirs, files in os.walk(final_dir + biomd_id):
         newfiles.extend(files)
-    
+
     deletedFiles = oldfiles
     for filename in newfiles:
         if filename in oldfiles:
@@ -54,15 +55,17 @@ def getNewMetadata(biomd, oldmetadata, master):
             if "sedml" in filename:
                 copasiversion = filename.replace("sedml", "cps")
                 if copasiversion in files:
-                    entry = {"name": filename, "description": "Newly-generated SED-ML file, based on original COPASI save file."}
+                    entry = {"name": filename,
+                             "description": "Newly-generated SED-ML file, based on original COPASI save file."}
                 else:
-                    entry = {"name": filename, "description": "Auto-generated \"template\" SED-ML file."}                    
+                    entry = {"name": filename, "description": "Auto-generated \"template\" SED-ML file."}
             else:
                 if ".ode" in filename:
                     entry = {"name": filename, "description": "Auto-generated ODE file for XPP."}
-                else:                    
+                else:
                     # LS TODO:  Find old descriptions of renamed files.
-                    entry = {"name": filename, "description": "Auto-generated file from Center for Reproducible Biomedical Modeling curation work."}
+                    entry = {"name": filename,
+                             "description": "Auto-generated file from Center for Reproducible Biomedical Modeling curation work."}
                 # print("Adding file:", biomd, filename)
         if filename == master:
             metadata["files"]["main"].append(entry)
@@ -70,37 +73,35 @@ def getNewMetadata(biomd, oldmetadata, master):
             metadata["files"]["additional"].append(entry)
 
     for filename in deletedFiles:
-        pass 
-        #print("Deleting file:", biomd, filename)
-        
+        pass
+        # print("Deleting file:", biomd, filename)
+
     return metadata
 
 
-def uploadBiomodelFilesTo(biomd, folder, auth, metadata):
+def upload_model_files(biomd_id, submission_folder, auth, metadata):
     upload = {
-	    "Content-Type": "application/json", 
-	    "Accept": "application/json", 
-	    "Authorization": "Bearer " + auth, 
-	    "SubmissionFolder": folder
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer " + auth,
+        "SubmissionFolder": submission_folder
     }
-    newfiles = []
-    for root, dirs, files in os.walk(final_dir + biomd):
-        newfiles.extend(files)
+    new_files = []
+    for root, dirs, files in os.walk(final_dir + biomd_id):
+        new_files.extend(files)
     for filename in files:
-        files = {"file": open(final_dir + biomd + "/" + filename,"rb")}
+        files = {"file": open(final_dir + biomd_id + "/" + filename, "rb")}
         ret = requests.post(BM_UPLOAD, headers=upload, files=files)
         ret.raise_for_status()
         # print(ret.json())
     ret = requests.post(root_biomodels + "api/submission/update/", headers=upload, json=metadata)
     ret.raise_for_status()
-    # print(ret)
-        
-    
+    print(ret)
+
 
 f = open(AUTH_FILE, "r")
 credentials = json.load(f)
 f.close()
-
 
 # get_access_credentials = requests.post(
 #     "https://wwwdev.ebi.ac.uk/biomodels/api/login", json = credentials)
@@ -113,18 +114,17 @@ f = open(masters_filename, "r")
 masters = json.load(f)
 f.close()
 
-
 for root, dirs, files in os.walk(final_dir):
     break
 
 sub_ids = {"BIOMD0000000005": "MODEL0000000011", "BIOMD0000000011": "MODEL0000000015"}
-for biomd in dirs:
-    if biomd == "BIOMD0000000005":
-        oldmd = requests.get(prod_biomodels + biomd, params={"format": "json"})
+for biomd_id in dirs:
+    if biomd_id == "BIOMD0000000005":
+        oldmd = requests.get(prod_biomodels + biomd_id, params={"format": "json"})
         oldmetadata = oldmd.json()
-        
-        newmetadata = getNewMetadata(biomd, oldmd.json(), masters[biomd])
-        folder = biomd + "-" + str(uuid.uuid4())
+
+        new_metadata = get_new_metadata(biomd_id, oldmd.json(), masters[biomd_id])
+        folder = biomd_id + "-" + str(uuid.uuid4())
         folder = str(uuid.uuid4())
-        newmetadata["submissionId"] = sub_ids[newmetadata["publicationId"]]
-        uploadBiomodelFilesTo(biomd, folder, credentials["access_token"], newmetadata)
+        new_metadata["submissionId"] = sub_ids[new_metadata["publicationId"]]
+        upload_model_files(biomd_id, folder, credentials["access_token"], new_metadata)
