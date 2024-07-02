e911d736d7eb40cd014d278fd4ffdd1748c1bef8
Lucian Smith <lpsmith@uw.edu>
2024-07-02 09:36:14 -0700

    Updated with minor updates and corrections.
    Now points at my own directory again, and lets me loop through biomodels by number instead of by directory.  Also fixes a bug where one model had zero supplemental files.
    
    Also couldn't tell whether 'modelling_approach' or 'modellingApproach' is correct, so set them both.

--- pyguide/run_update.py
+++ pyguide/run_update.py
@@ -10,7 +10,8 @@ import json
 import requests
 import uuid
 
-final_dir = "final/"
+# final_dir = "final/"
+final_dir = r"C:\Users\Lucian\Desktop\temp-biomodels\final/"
 masters_filename = "all_masters.json"
 root_biomodels = "https://wwwdev.ebi.ac.uk/biomodels/"
 
@@ -22,26 +23,24 @@ prod_biomodels = "https://wwwdev.ebi.ac.uk/biomodels/"
 
 def get_new_metadata(biomd_id, old_metadata, master):
     metadata = {}
-    for key in ["name", "description", "publication"]:
+    for key in ["name", "description", "publication", "submissionId", "publicationId", "format"]:
         metadata[key] = old_metadata[key]
-    metadata["submissionId"] = old_metadata["submissionId"]
-    metadata["publicationId"] = old_metadata["publicationId"]
-    metadata[
-        "readme_submission"] = "Curation updates for SED-ML and validity, from the Center for Reproducible Biomedical Modeling, via Lucian Smith."
+    metadata["readme_submission"] = "Curation updates for SED-ML and validity, from the Center for Reproducible Biomedical Modeling, via Lucian Smith."
     metadata["other_info"] = "SBML Model Format"
     metadata["isMetadataSubmission"] = False
-    metadata["isAmend"] = False
+    metadata["isAmend"] = True
     metadata["comment"] = "A variety of manual and automated updates for all extant curated biomodels, 2024."
-    metadata["format"] = old_metadata["format"]
     metadata["files"] = {"main": [], "additional": []}
     if "modellingApproach" in old_metadata and old_metadata["modellingApproach"] is not None:
         metadata["modelling_approach"] = old_metadata["modellingApproach"]["name"]
+        metadata["modellingApproach"] = old_metadata["modellingApproach"]["name"]
 
     # Now update all the files:
     oldfiles = {}
     for key in ["main", "additional"]:
-        for entry in old_metadata["files"][key]:
-            oldfiles[entry["name"]] = entry["description"]
+        if key in old_metadata["files"]:
+            for entry in old_metadata["files"][key]:
+                oldfiles[entry["name"]] = entry["description"]
 
     newfiles = []
     for root, dirs, files in os.walk(final_dir + biomd_id):
@@ -61,6 +60,13 @@ def get_new_metadata(biomd_id, old_metadata, master):
                              "description": "Newly-generated SED-ML file, based on original COPASI save file."}
                 else:
                     entry = {"name": filename, "description": "Auto-generated \"template\" SED-ML file."}
+            elif "curation_image" in filename:
+                entry = {"name": filename,
+                         "description": "Plotted results from original curation."}
+            elif "curation_notes" in filename:
+                entry = {"name": filename,
+                         "description": "Curator's notes from original curation."}
+                
             else:
                 if ".ode" in filename:
                     entry = {"name": filename, "description": "Auto-generated ODE file for XPP."}
@@ -95,7 +101,7 @@ def upload_model_files(biomd_id, submission_folder, auth, metadata):
         ret.raise_for_status()
         print(ret.json())
     ret = requests.post(prod_biomodels + "api/submission/update/", headers=upload, json=metadata)
-    ret.raise_for_status()
+    # ret.raise_for_status()
     print(ret)
 
 
@@ -117,8 +123,14 @@ f.close()
 for root, dirs, files in os.walk(final_dir):
     break
 
-sub_ids = {"BIOMD0000000005": "MODEL0000000011", "BIOMD0000000011": "MODEL0000000015"}
-for biomd_id in dirs:
+# sub_ids = {"BIOMD0000000005": "MODEL0000000011", "BIOMD0000000011": "MODEL0000000015"}
+for i in range(1, 10): #1081):
+# for i in skipped:
+    if i in [649, 694, 992, 993, 1049, 1050, 1051, 1066, 1067, 1068, 1069, 1070, 1071, 1073, 1074, 1075, 1076, ]: #Don't exist or aren't SBML
+        continue
+    num = f'{i:010d}'
+    biomd_id = "BIOMD" + num
+    print("Updating biomodel", biomd_id)
     oldmd = requests.get(prod_biomodels + biomd_id, params={"format": "json"})
     oldmetadata = oldmd.json()
 
