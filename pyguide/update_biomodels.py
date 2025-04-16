764e88df72dc40904856eadc3b68ec3bcc6b18b6
Tung Nguyen <nvntung@gmail.com>
2025-04-16 12:19:10 +0200

    Add the method docs and better the codes

--- pyguide/update_biomodels.py
+++ pyguide/update_biomodels.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 12 16:14:37 2024
+Updates a given model
 
 @author:
     Lucian Smith
@@ -55,38 +55,55 @@ f.close()
 headers = {
     "Authorization": "Bearer " + credentials["access_token"]
 }
+params = {
+    "format": "json"
+}
+timeout = (30, None)
 
 def update_model_filenames(updated_filenames, _metadata):
+    """ Update the name of the model files after uploading """
+
     all_model_files = _metadata["files"]["main"] + _metadata["files"]["additional"]
     for file in all_model_files:
-        file["name"] = updated_filenames[file["name"]]
-
+        if file["name"] in updated_filenames:
+            file["name"] = updated_filenames[file["name"]]
 
 def get_old_metadata(_model_id):
-    URL = root_biomodels + "api/model/" + _model_id + "?format=json"
-    old_md = requests.get(URL, headers=headers)
+    """ Get the old/current metadata of a given model id in JSON format """
+
+    _url = root_biomodels + "api/model/" + _model_id
+    old_md = requests.get(_url, headers=headers, timeout=timeout,
+                          params=params,
+                          allow_redirects=True)
     _old_metadata = old_md.json()
     return _old_metadata
 
 
 def get_new_metadata(_old_metadata):
+    """ Populate the new metadata from the old metadata """
+
     metadata = {"submissionId": _old_metadata["submissionId"]}
     if "publicationId" in _old_metadata:
         metadata["publicationId"] = _old_metadata["publicationId"]
-    for key in ["name", "description", "publication"]:
+
+    for key in ["name", "description"]:
         metadata[key] = _old_metadata[key]
-    metadata["readme_submission"] = "Updated the model revision via REST API."
-    metadata["other_info"] = "SBML Model Format"
-    metadata["isMetadataSubmission"] = False
+    # metadata["description"] = metadata["description"] + ". Updated: test the recent changes."
+    metadata["readme_submission"] = "Updated the model revision via REST API: added an image as the additional file."
+    # metadata["other_info"] = "Dynamic Modelling"
+    
     if "modellingApproach" in _old_metadata and _old_metadata["modellingApproach"] is not None:
         metadata["modelling_approach"] = _old_metadata["modellingApproach"]["name"]
     elif "modelling_approach" in _old_metadata and _old_metadata["modelling_approach"] is not None:
         metadata["modelling_approach"] = _old_metadata["modelling_approach"]
-
     # metadata["modelling_approach"] = "Deep Learning"
-    metadata["isMetadataSubmission"] = True
+
+    metadata["isMetadataSubmission"] = False
+
     metadata["isAmend"] = True
-    metadata["comment"] = "Corrected the annotations and added the article in PDF."
+
+    metadata["comment"] = "Added the manuscript and corrected the annotations."
+
     metadata["files"] = {"main": _old_metadata["files"]["main"], "additional": []}
     if "additional" in _old_metadata["files"]:
         metadata["files"]["additional"] = _old_metadata["files"]["additional"]
@@ -121,29 +138,26 @@ def update_model(_model_id, metadata, _headers):
     folder = str(uuid.uuid4())
     _headers["SubmissionFolder"] = folder
     new_files = []
+    _files = []
     for _root, _dirs, _files in os.walk(final_dir + _model_id):
         new_files.extend(_files)
     corrected_filenames = {}
     for filename in _files:
         uploaded_files = {"file": open(final_dir + _model_id + "/" + filename, "rb")}
-        ret = requests.post(DOWNUP_SVR + "services/upload",
-                            headers=_headers, files=uploaded_files)
+        _url = file_server + "services/upload"
+        ret = requests.post(_url, headers=_headers, files=uploaded_files, timeout=timeout)
         ret.raise_for_status()
         corrected_filenames[filename] = ret.json()["after_uploaded_filename"]
     update_model_filenames(corrected_filenames, metadata)
-    params = {
-        "format": "json"
-    }
     ret = requests.post(root_biomodels + "api/submission/update",
                         headers=_headers, params=params, json=metadata)
     ret.raise_for_status()
-    # print(ret)
     print(json.dumps(ret.json()))
 
 
+dirs = []
 for root, dirs, files in os.walk(final_dir):
     break
-
 for model_id in dirs:
 
     if model_id != "MODEL1805180007":
