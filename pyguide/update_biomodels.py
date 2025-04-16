e2fa3a2085deb74208b7e08f22d7e4151163ff0b
Tung Nguyen <nvntung@gmail.com>
2025-04-16 12:22:00 +0200

    Improve the codes

--- pyguide/update_biomodels.py
+++ pyguide/update_biomodels.py
@@ -10,48 +10,44 @@ Updates a given model
 import os
 import json
 import sys
-
-import requests
 import uuid
+import requests
 
 if len(sys.argv) <= 1:
     print("Please specify a server. Otherwise, we use the local server.")
-    server = "local"
+    SERVER = "local"
 else:
-    server = sys.argv[1]
-if not server:
+    SERVER = sys.argv[1]
+if not SERVER:
     print("Please specify a server. Otherwise, we use the local server.")
 # hardcode server var here
-server = "local"
-if not server:
-    server = "local"
-
-if "local" == server:
-    file_server = "http://localhost:7001/biomodels/"
-    final_dir = r"/Users/tnguyen/ownCloud/EBI/biomodels/api-submission/models4test/wwwlocal/"
-    masters_filename = r"/Users/tnguyen/ownCloud/EBI/biomodels/api-submission/pyguide/all_masters.json"
-    root_biomodels = "http://localhost:8080/biomodels/"
-elif "dev" == server:
-    file_server = "https://wwwdev.ebi.ac.uk/biomodels/"
-    final_dir = r"/Users/tnguyen/ownCloud/EBI/biomodels/api-submission/models4test/wwwdev/"
-    masters_filename = r"C:\Users\Lucian\Desktop\temp-biomodels\all_masters.json"
-    root_biomodels = "https://wwwdev.ebi.ac.uk/biomodels/"
+if not SERVER:
+    SERVER = "local"
+PRJ_ROOT = "/Users/tnguyen/ownCloud/EBI/biomodels/api-submission"
+if "local" == SERVER:
+    CRED_FILEPATH = "../credentials/local-khang.json"
+    FILE_SERVER = "http://localhost:7001/biomodels/"
+    MODEL_DIR = PRJ_ROOT + "/models4test/wwwlocal/"
+    MASTERS_FILEPATH = PRJ_ROOT + "/pyguide/all_masters.json"
+    BM_URL = "http://localhost:8080/biomodels/"
+elif "dev" == SERVER:
+    CRED_FILEPATH = "../credentials/wwwdev-khang.json"
+    FILE_SERVER = "https://wwwdev.ebi.ac.uk/biomodels/"
+    MODEL_DIR = PRJ_ROOT + "/models4test/wwwdev/"
+    MASTERS_FILEPATH = PRJ_ROOT + "/pyguide/all_masters.json"
+    BM_URL = "https://wwwdev.ebi.ac.uk/biomodels/"
 else:
-    final_dir = r"/Users/tnguyen/ownCloud/EBI/biomodels/temp-biomodels/final/"
-    masters_filename = r"C:\Users\Lucian\Desktop\temp-biomodels\all_masters.json"
-    root_biomodels = "https://www.ebi.ac.uk/biomodels/"
-    file_server = "https://www.ebi.ac.uk/biomodels/"
+    CRED_FILEPATH = "../credentials/www-tnguyen.json"
+    MODEL_DIR = r"/Users/tnguyen/ownCloud/EBI/biomodels/temp-biomodels/final/"
+    MASTERS_FILEPATH = PRJ_ROOT + "/pyguide/all_masters.json"
+    BM_URL = "https://www.ebi.ac.uk/biomodels/"
+    FILE_SERVER = "https://www.ebi.ac.uk/biomodels/"
 
 
-f = open("../credentials/local-khang.json", "r")
-# f = open("../credentials/wwwdev-khang.json", "r")
-credentials = json.load(f)
-f.close()
-
-f = open(masters_filename, "r")
-masters = json.load(f)
-f.close()
-
+with open(CRED_FILEPATH, "r", encoding="utf-8") as file:
+    credentials = json.load(file)
+with open(MASTERS_FILEPATH, "r", encoding="utf-8") as file:
+    masters = json.load(file)
 headers = {
     "Authorization": "Bearer " + credentials["access_token"]
 }
@@ -64,14 +60,14 @@ def update_model_filenames(updated_filenames, _metadata):
     """ Update the name of the model files after uploading """
 
     all_model_files = _metadata["files"]["main"] + _metadata["files"]["additional"]
-    for file in all_model_files:
-        if file["name"] in updated_filenames:
-            file["name"] = updated_filenames[file["name"]]
+    for mfile in all_model_files:
+        if mfile["name"] in updated_filenames:
+            mfile["name"] = updated_filenames[mfile["name"]]
 
 def get_old_metadata(_model_id):
     """ Get the old/current metadata of a given model id in JSON format """
 
-    _url = root_biomodels + "api/model/" + _model_id
+    _url = BM_URL + "api/model/" + _model_id
     old_md = requests.get(_url, headers=headers, timeout=timeout,
                           params=params,
                           allow_redirects=True)
@@ -88,13 +84,17 @@ def get_new_metadata(_old_metadata):
 
     for key in ["name", "description"]:
         metadata[key] = _old_metadata[key]
-    # metadata["description"] = metadata["description"] + ". Updated: test the recent changes."
-    metadata["readme_submission"] = "Updated the model revision via REST API: added an image as the additional file."
+    # metadata["description"] = metadata["description"] + ". Added the recent
+    # changes."
+    metadata["readme_submission"] = "\
+    Updated the model via REST API: added an image as the additional file."
     # metadata["other_info"] = "Dynamic Modelling"
-    
-    if "modellingApproach" in _old_metadata and _old_metadata["modellingApproach"] is not None:
+
+    if ("modellingApproach" in _old_metadata
+            and _old_metadata["modellingApproach"] is not None):
         metadata["modelling_approach"] = _old_metadata["modellingApproach"]["name"]
-    elif "modelling_approach" in _old_metadata and _old_metadata["modelling_approach"] is not None:
+    elif ("modelling_approach" in _old_metadata
+          and _old_metadata["modelling_approach"] is not None):
         metadata["modelling_approach"] = _old_metadata["modelling_approach"]
     # metadata["modelling_approach"] = "Deep Learning"
 
@@ -111,14 +111,6 @@ def get_new_metadata(_old_metadata):
     #     "name": "1-s2.0-S2211124722016138-main.pdf",
     #     "description": "PDF file of the publication paper"
     # })
-    # metadata["files"]["additional"].append({
-    #     "name": "article.pdf",
-    #     "description": "PDF file of the publication paper"
-    # })
-    # metadata["files"]["additional"].append({
-    #     "name": "article.pdf",
-    #     "description": "PDF file of the publication paper"
-    # })
     if "format" in _old_metadata:
         metadata["format"] = _old_metadata["format"]
     else:
@@ -129,42 +121,49 @@ def get_new_metadata(_old_metadata):
     if "publication" in _old_metadata:
         metadata["publication"] = _old_metadata["publication"]
 
-    # Add contributor role
-    metadata["contributorRole"] = "Annotation Curator"
+    # Add contributor role: https://www.ebi.ac.uk/biomodels/api/contributor/role
+    metadata["contributorRole"] = "Curator"
     return metadata
 
 
-def update_model(_model_id, metadata, _headers):
+def update_model(_model_id, metadata):
+    """ Update the model given in _model_id with metadata """
+
     folder = str(uuid.uuid4())
+    _headers = headers
     _headers["SubmissionFolder"] = folder
     new_files = []
     _files = []
-    for _root, _dirs, _files in os.walk(final_dir + _model_id):
+    for _root, _dirs, _files in os.walk(MODEL_DIR + _model_id):
         new_files.extend(_files)
     corrected_filenames = {}
     for filename in _files:
-        uploaded_files = {"file": open(final_dir + _model_id + "/" + filename, "rb")}
-        _url = file_server + "services/upload"
-        ret = requests.post(_url, headers=_headers, files=uploaded_files, timeout=timeout)
+        with open(MODEL_DIR + _model_id + "/" + filename, "rb") as _file:
+            uploaded_files = {"file": _file}
+            _url = FILE_SERVER + "services/upload"
+            ret = requests.post(_url, headers=_headers, files=uploaded_files,
+                                timeout=timeout)
         ret.raise_for_status()
         corrected_filenames[filename] = ret.json()["after_uploaded_filename"]
     update_model_filenames(corrected_filenames, metadata)
-    ret = requests.post(root_biomodels + "api/submission/update",
-                        headers=_headers, params=params, json=metadata)
+    _url = BM_URL + "api/submission/update"
+    ret = requests.post(_url, headers=_headers, params=params,
+                        json=metadata, timeout=timeout)
     ret.raise_for_status()
     print(json.dumps(ret.json()))
 
 
-dirs = []
-for root, dirs, files in os.walk(final_dir):
-    break
-for model_id in dirs:
+def do_update_model(m_id):
+    """ Do update the given model """
+    old_mt = get_old_metadata(m_id)
+    new_mt = get_new_metadata(old_mt)
+    print(json.dumps(new_mt))
+    update_model(m_id, new_mt)
 
-    if model_id != "MODEL1805180007":
-        # "MODEL2107080001":
-        pass
-    else:
-        old_metadata = get_old_metadata(model_id)
-        new_metadata = get_new_metadata(old_metadata)
-        print(json.dumps(new_metadata))
-        update_model(model_id, new_metadata, headers)
+
+dirs = []
+for root, dirs, files in os.walk(MODEL_DIR):
+    break
+print(dirs)
+model_id = sys.argv[2]
+do_update_model(model_id)
